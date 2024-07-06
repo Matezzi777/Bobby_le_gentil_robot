c3038cf5078d484cc4e7af54f64616334d91035a
Matezzi777 <matezzi777@gmail.com>
2024-07-06 04:03:34 +0200

    code cleaned

--- main.py
+++ main.py
@@ -28,16 +28,27 @@ async def on_ready():
     return print(f"\n... est connecté.\nRock n'Roll !")
 
 #====================== COMMANDES =======================
-
+#Renvoit PONG si le bot est connecté
 @bot.slash_command(guild_ids=serveurs, name="ping", description="PONG !")
 async def ping(interaction: discord.Interaction):
     embed = BotEmbed(title="PONG", colour=discord.Colour.green())
     embed.remove_footer()
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
+#Renvoit une présentation du bot
 @bot.slash_command(guild_ids=serveurs, name="hello", description="Dis bonjour !")
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hey {interaction.user.mention}!\nThis is a slash command!", ephemeral=True)
+    await interaction.response.send_message(f"Bonjour maître {interaction.user.mention} !\nC'est un plaisir de faire votre connaissance!\n\nN'hésitez pas à faire appel à moi pour préparer vos parties de civilisation, garder une trace de vos résultats et de vos statistiques dans différent jeux.\n\nSi vous avez une idée pour m'améliorer, utilisez la commande ***/feedback*** pour faire une suggestion !", ephemeral=True)
+
+#Distribue des leaders de civilization vi aux joueurs présents dans le salon vocal
+@bot.slash_command(guild_ids=serveurs, name="civ_draft", description="Distribue aléatoirement des leaders aux joueurs dans le salon vocal.")
+async def civ_draft(interaction: discord.Interaction):
+    ...
+
+#Renvoit un formulaire pour émettre des suggestions d'amélioration du bot
+@bot.slash_command(guild_ids=serveurs, name="feedback", description="Formulaire pour envoyer une suggestion d'amélioration.")
+async def feedback(interaction: discord.Interaction):
+    ...
 
 #========================= RUN ==========================
 bot.run(TOKEN)
